eb3d681c8236020823c41b94b75616b4dcc4f2a2
DenHax <whatxpw@gmail.com>
2025-05-06 01:31:32 +0300

    feat(separator): add parsing to get info about separator and repeating tries

--- main.go
+++ main.go
@@ -4,27 +4,69 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-func dotSepar(words []string) {
+func dotSepar(words []string, count int) {
 	for _, word := range words {
 		fmt.Print(word)
-		fmt.Print(".")
+		for range count {
+			fmt.Print(".")
+		}
 	}
 }
 
-func commaSepar(words []string) {
+func commaSepar(words []string, count int) {
 	for _, word := range words {
 		fmt.Print(word)
-		fmt.Print(",")
+		for range count {
+			fmt.Print(",")
+		}
 	}
 }
 
-func semiSepar(words []string) {
+func semiSepar(words []string, count int) {
 	for _, word := range words {
 		fmt.Print(word)
-		fmt.Print(":")
+		for range count {
+			fmt.Print(":")
+		}
 	}
+}
+
+func parseSeparator(input string) (sep string, count int) {
+	input = strings.TrimSpace(input)
+	if input == "" {
+		return
+	}
+
+	var digits strings.Builder
+	var separator strings.Builder
+	hasDigits := false
+	hasSeparator := false
+
+	for _, r := range input {
+		if unicode.IsDigit(r) {
+			digits.WriteRune(r)
+			hasDigits = true
+		} else if !unicode.IsSpace(r) {
+			separator.WriteRune(r)
+			hasSeparator = true
+		}
+	}
+
+	if hasDigits {
+		if num, err := strconv.Atoi(digits.String()); err == nil && num > 0 {
+			count = num
+		}
+	}
+	if hasSeparator {
+		sep = string([]rune(separator.String())[0])
+	}
+
+	return
 }
 
 func main() {
@@ -35,8 +77,8 @@ func main() {
 	defer out.Flush()
 
 	var n int
-	words := make([]string, 0)
-	var separator string
+	arr := make([]string, 0)
+	var str string
 
 	fmt.Println("Введите количество слов:")
 	fmt.Fscan(in, &n)
@@ -46,17 +88,21 @@ func main() {
 	for range n {
 		var word string
 		fmt.Fscan(in, &word)
-		words = append(words, word)
+		arr = append(arr, word)
 	}
 
-	fmt.Println("Введите сепаратор:")
-	fmt.Fscan(in, &separator)
-	in.ReadString('\n')
+	fmt.Println("Введите сепаратор и количество повторений (например, '.3' или '2,' или ':'):")
 
-	if separator == "." {
-		dotSepar(words)
-	} else if separator == "," {
-		commaSepar(words)
+	in.ReadString('\n')
+	str, _ = in.ReadString('\n')
+	str = strings.TrimSpace(str)
+
+	sep, m := parseSeparator(str)
+
+	if sep == "." {
+		dotSepar(arr, m)
+	} else if sep == "," {
+		commaSepar(arr, m)
 	} else {
 		fmt.Fprint(out, "Введены некорректные данные")
 	}
